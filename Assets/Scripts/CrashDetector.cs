662172e5da9b63ab71125024cb32c1e4d66c2094
odrmasliuk <odr.masliuk@gmail.com>
2022-11-02 20:08:04 +0100

    Lecture 11 | Done

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,12 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _crashVFX;
     [SerializeField] private float _reloadDelay;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Ground"))
         {
+            _crashVFX.Play();
             Invoke("ReloadLevel", _reloadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,12 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _finishVFX;
     [SerializeField] private float _reloadDelay;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Player")) 
         {
+            _finishVFX.Play();
             Invoke("ReloadLevel", _reloadDelay);
         }
     }
