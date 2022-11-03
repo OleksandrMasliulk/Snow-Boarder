47cc9b5109924a217085d570faf56ae3cc51f576
odrmasliuk <odr.masliuk@gmail.com>
2022-11-03 20:22:43 +0100

    Lecture 15 | Done

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class CrashDetector : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _crashVFX;
     [SerializeField] private float _reloadDelay;
+
+    [SerializeField] private ParticleSystem _crashVFX;
+
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip _crashSFX;
+
+    private void Awake() 
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Ground"))
         {
             _crashVFX.Play();
+            _audioSource.PlayOneShot(_crashSFX);
             Invoke("ReloadLevel", _reloadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class FinishLine : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _finishVFX;
     [SerializeField] private float _reloadDelay;
+
+    [SerializeField] private ParticleSystem _finishVFX;
+
+    private AudioSource _audioSource;
+
+    private void Awake() 
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Player")) 
         {
             _finishVFX.Play();
+            _audioSource.Play();
             Invoke("ReloadLevel", _reloadDelay);
         }
     }
